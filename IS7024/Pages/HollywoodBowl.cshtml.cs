090f523692fb2474b2f32b9b9f8784b690c41d11
huberr3 <54744801+huberr3@users.noreply.github.com>
2020-11-23 17:18:16 -0500

    Cleared out commented-out code.

--- IS7024/Pages/HollywoodBowl.cshtml.cs
+++ IS7024/Pages/HollywoodBowl.cshtml.cs
@@ -20,23 +20,11 @@ namespace IS7024.Pages
         {
             using (var webClient = new WebClient())
             {
-               /* string sunJSON = webClient.DownloadString("https://api.sunrise-sunset.org/json?lat=34.1122&lng=118.3391&date=2020-11-02");
-                var sun = QuickTypeSun.Sun.FromJson(sunJSON);
-                var results = sun.Results.Sunset;
-                string s = results;
-                string[] vars = s.Split(':');
-                var newtime = Convert.ToInt16(vars[0]);
-                int realtime = newtime + 9 - 12;
-                string itstime = "~" + realtime + ":" + vars[1] + " PM";
-                ViewData["sun"] = itstime;*/
-
-
+               
                 string jsonString = webClient.DownloadString("https://api.songkick.com/api/3.0/venues/7211/calendar.json?apikey=Y77nbW56nkfpIpw9");
                 JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("SongkickSchema.json"));
                 JObject jsonObject = JObject.Parse(jsonString);
                 IList<string> validationEvents = new List<string>();
-                //if (jsonObject.IsValid(schema, out validationEvents))
-                //{
                     var hBowl = HBowl.FromJson(jsonString);
                     List<Event> events = hBowl.ResultsPage.Results.Event.ToList();
                 foreach (Event each_event in events)
@@ -63,20 +51,6 @@ namespace IS7024.Pages
 
 
             }
-
-
-                
-
-                //}
-                //else
-                {
-                  //  foreach (string evt in validationEvents)
-                    {
-                    //    Console.WriteLine(evt);
-                    }
-                    //ViewData["events"] = new List<Event>();
-                }
-
-            }
         }
     }
+}
--- IS7024/Pages/LudlowGarage.cshtml.cs
+++ IS7024/Pages/LudlowGarage.cshtml.cs
@@ -17,29 +17,13 @@ namespace IS7024.Pages
         {
             using (var webClient = new WebClient())
             {
-               /* string sunJSON = webClient.DownloadString("https://api.sunrise-sunset.org/json?lat=39.7970&lng=83.8255&date=2020-11-02");
-                var sun = QuickTypeSun.Sun.FromJson(sunJSON);
-                var results = sun.Results.Sunset;
-                string s = results;
-                string[] vars = s.Split(':');
-                var newtime = Convert.ToInt16(vars[0]);
-                int realtime = newtime + 6 - 12;
-                string itstime = "~" + realtime + ":" + vars[1] + " PM";
-                ViewData["sun"] = itstime; */
-                
-
                 string jsonString = webClient.DownloadString("https://api.songkick.com/api/3.0/venues/62388/calendar.json?apikey=Y77nbW56nkfpIpw9");
                 JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("SongkickSchema.json"));
                 JObject jsonObject = JObject.Parse(jsonString);
                 IList<string> validationEvents = new List<string>();
-               // if (jsonObject.IsValid(schema, out validationEvents))
-                //{
                     var rootObject = RootObject.FromJson(jsonString);
                     List<Event> events = rootObject.ResultsPage.Results.Event.ToList();
-                   // ViewData["events"] = events;
-
-               // }
-                //else
+              
                 {
                     foreach (Event evt in events)
                     {
@@ -61,7 +45,7 @@ namespace IS7024.Pages
                         string itstime = "~" + realtime + ":" + vars[1] + " PM";
                         evt.Sunset = itstime;
                     }
-                    //ViewData["events"] = new List<Event>();
+                    
                     ViewData["events"] = events;
                 }
 
